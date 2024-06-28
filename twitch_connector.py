45ca190c894c2705d409f417e0e73edbb06fc5a4
Pd George <pdgeorge.surrealism@gmail.com>
2024-06-28 18:38:20 +1000

    Ingests messages from Twitch

--- twitch_connector.py
+++ twitch_connector.py
@@ -10,12 +10,13 @@ SERVER = "irc.chat.twitch.tv"
 PORT = 6667
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 NICKNAME = os.getenv("BOT_NICK")
-# CHANNEL = os.getenv("CHANNEL")
-CHANNEL = "megaa"
+CHANNEL = os.getenv("CHANNEL")
+# CHANNEL = "manukab"
 
-class Bot():
+class TwitchBot():
     
     def __init__(self):
+        print(f"Joining: {CHANNEL}")
         self.sock = socket.socket()
         self.sock.connect((SERVER, PORT))
         self.join()
@@ -28,17 +29,20 @@ class Bot():
     def listen_for_msg(self):
         resp = self.sock.recv(2048).decode('utf-8')
         if resp.startswith('PING'):
+            print("===========================================")
+            print("pong")
+            print("===========================================")
             self.sock.send("PONG\n".encode('utf-8'))
-            return
+            return 'PING'
         elif len(resp) > 0:
             resp = demojize(resp)
             return resp
 
 def main():
-    bot = Bot()
+    twitch_bot = TwitchBot()
     print("started?")
     while True:
-        resp = bot.listen_for_msg()
+        resp = twitch_bot.listen_for_msg()
         print(resp)
 
 if __name__ == "__main__":
